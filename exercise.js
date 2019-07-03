ff95179084acd61c1058f842ad1439f6482c0b5a
Khrysty <51955462+Khrysty@users.noreply.github.com>
2019-07-03 15:53:17 +0300

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,69 @@
+// Task 1 https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript?fbclid=IwAR1A_XqJiui4qVreTHiyqjNe3PmiAyiEHsHHBic9MAsSCAg4P61UAX_LUSI
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    var sif = new SmallestIntegerFinder () ;
+    var allNumbers = [34, 15, 88, 2];
+    var number[0] = 34;
+    var number[1] = 15;
+    var number[2] = 88;
+    var number[3] = 2;
+    allNumbers.lenght =[0], [1], [2];
+    
+    class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    var sif = new SmallestIntegerFinder () ;
+    var allNumbers = [34, -345, -1, 100];
+    var number[0] = 34;
+    var number[1] = -345;
+    var number[2] = -1;
+    var number[3] = 100;
+    allNumbers.lenght = [0], [1], [3];
+    
+    АБО
+    
+    class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    var sif = new SmallestIntegerFinder () ;
+    var firstNumbers = [34, 15, 88, 2];
+    firstNumbers.lenght = 2
+    
+    
+    
+    // Task 2 https://www.codewars.com/kata/training-js-number-4-basic-data-types-array/train/javascript
+
+function getLength(arr){
+  
+  return arr.lenght
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr.unshift
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr.push
+}
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+  
+  return arr
+}
+function popElement(arr){
+  //pop an element from arr
+  
+  return ?
+}
+    
+    // Task 3 https://www.codewars.com/kata/beginner-lost-without-a-map/train/javascript
+    function maps(x){
+return x.map (z => z*2 );
+}
+
+// Task 4 https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript?fbclid=IwAR3vDDr5GYt-O5YNs64mw8C3rJ-NYv6GIJ8WBpAed4EkKxiiixGnUjk2KW0
+function DNAtoRNA(dna) {
+return dna.replace (/T/g, 'U') 
+}
+
+
+
